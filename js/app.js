a2911e09bb533d178707df7e85800b31cb407715
Eli <eligitport@gmail.com>
2020-02-01 13:50:34 +0000

    slider.html add

---
+++ js/app.js
@@ -0,0 +1,8 @@
+(function() {
+  // NB: For more info | Goto https://glidejs.com/ and to options
+  // invoke the Glidejs instance
+  new Glide('.glide', {
+    type: 'carousel',
+    autoplay: 2500
+  }).mount();
+})();
